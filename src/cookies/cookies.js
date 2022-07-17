0d47fb1500c423ef7275498fa1a516945892bb0c
thanhnth-0124 <nguyen.thi.hai.thanh@sun-asterisk.com>
2022-07-17 12:46:00 +0700

    delete cookies when logout

--- src/cookies/cookies.js
+++ src/cookies/cookies.js
@@ -19,3 +19,9 @@ export function getCookie(cname) {
   }
   return "";
 }
+
+export function deleteCookies() {
+  var allCookies = document.cookie.split(";");
+  for (var i = 0; i < allCookies.length; i++)
+    document.cookie = allCookies[i] + "=;expires=" + new Date(0).toUTCString();
+}
